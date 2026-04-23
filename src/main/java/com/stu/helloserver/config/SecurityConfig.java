1e64e1fffe07b34d83a82bb235a14e26e4803bc1
PromiseX823 <3082988937@qq.com>
2026-04-23 21:27:12 +0800

    第八周

---
+++ src/main/java/com/stu/helloserver/config/SecurityConfig.java
@@ -0,0 +1,36 @@
+package com.stu.helloserver.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .cors(withDefaults())
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .authorizeHttpRequests(auth -> auth
+                        .antMatchers(HttpMethod.POST,"/api/users").permitAll()
+                        .antMatchers(HttpMethod.POST, "/api/users/register").permitAll()
+                        .antMatchers(HttpMethod.POST,"/api/users/login").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable);
+
+        return http.build();
+    }
+}
--- src/main/java/com/stu/helloserver/config/WebConfig.java
+++ src/main/java/com/stu/helloserver/config/WebConfig.java
@@ -1,21 +1,11 @@
 package com.stu.helloserver.config;
 
-import com.stu.helloserver.interceptor.AuthInterceptor;
+//import com.stu.helloserver.interceptor.AuthInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-    @Override
-    public void addInterceptors(InterceptorRegistry registry){
-        registry.addInterceptor(new AuthInterceptor())
-                .addPathPatterns("/api/**")
-                .excludePathPatterns(
-                        "/api/users/login",
-                        "/api/users/register",
-                        "/api/users/*",
-                        "/api/users/*/detail"
-                );
-    }
+
 }
