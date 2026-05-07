5b75feb71b1d494a3ba03c32c1e1e40bfafa30bc
PromiseX823 <3082988937@qq.com>
2026-05-07 18:45:02 +0800

    10(2)

---
+++ src/main/java/com/stu/helloserver/controller/ChatController.java
@@ -0,0 +1,31 @@
+package com.stu.helloserver.controller;
+
+import com.stu.helloserver.common.Result;
+import com.stu.helloserver.model.dto.ChatRequestDTO;
+import com.stu.helloserver.model.vo.ChatResponseVO;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import com.stu.helloserver.service.ChatService;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@RestController
+@RequestMapping("/api/chat")
+public class ChatController {
+
+    private final ChatService chatService;
+
+    public ChatController(ChatService chatService) {
+        this.chatService = chatService;
+    }
+
+    @PostMapping
+    public Result<ChatResponseVO> chat(@RequestBody ChatRequestDTO requestDTO) {
+        String answer = chatService.chat(requestDTO.getMessage());
+
+        ChatResponseVO responseVO = new ChatResponseVO(requestDTO.getMessage(), answer);
+
+        return Result.success(responseVO);
+    }
+}
---
+++ src/main/java/com/stu/helloserver/model/dto/ChatRequestDTO.java
@@ -0,0 +1,7 @@
+package com.stu.helloserver.model.dto;
+import lombok.Data;
+
+@Data
+public class ChatRequestDTO {
+    private String message;
+}
